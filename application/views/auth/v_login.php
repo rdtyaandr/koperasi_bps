0bdfa0eae258580bb99eded3f64dbdf9f5cc1ef2
rdtyaandr <radltyaa260819@gmail.com>
2024-07-17 14:22:28 +0700

    ganti design login

--- application/views/auth/v_login.php
+++ application/views/auth/v_login.php
@@ -9,62 +9,39 @@
     <meta name="author" content="" />
     <title>Sistem Koperasi</title>
     <link href="<?= base_url() ?>material/css/styles.css" rel="stylesheet" />
+    <link href="<?= base_url() ?>material/css/form.css" rel="stylesheet" />
     <link rel="icon" type="image/x-icon" href="<?= base_url() ?>material/assets/img/icon.png" />
     <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.24.1/feather.min.js" crossorigin="anonymous"></script>
 </head>
 
-<body class="bg-primary">
-    <div id="layoutAuthentication">
-        <div id="layoutAuthentication_content">
-            <main>
-                <div class="container">
-                    <div class="login" data-login="<?= $this->session->flashdata('login'); ?>">
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-lg-5">
-                            <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                <div class="card-header justify-content-center">
-                                    <h3 class="font-weight-light my-4">Login</h3>
-                                </div>
-                                <div class="card-body">
-                                    <form action="<?= base_url('auth/process') ?>" method="post">
-                                        <div class="form-group">
-                                            <label class="small mb-1">Username</label>
-                                            <input class="form-control py-4" name="username" type="text" placeholder="Username" required />
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="small mb-1">Password</label>
-                                            <input class="form-control py-4" name="password" type="password" placeholder="Password" required/>
-                                        </div>
-                                        <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
-                                            <button type="submit" class="btn btn-primary btn-block" name="login">Login</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+<body class="text-center">
+    <main class="form-signin">
+        <form action="<?= base_url('auth/process') ?>" method="post">
+            <img class="mb-4" src="<?= base_url() ?>material/assets/img/icon.png" alt="" width="72" height="72">
+            <h1 class="h3 mb-3 fw-normal">Login Koperasi</h1>
+
+            <div class="form-floating">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username" autocomplete="off" required>
+            </div>
+            <div class="form-floating">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+            </div>
+
+            <button class="w-100 btn btn-lg btn-primary" type="submit" name="login">Login</button>
+        </form>
+        <footer class="footer mt-auto">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 small">
+                        Copyright &copy; BPS Jawa Timur <?= date('Y') ?>
                     </div>
                 </div>
-            </main>
-        </div>
-        <div id="layoutAuthentication_footer">
-            <footer class="footer mt-auto footer-dark">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-6 small">
-                            Copyright &copy; Naz Codes <?= date('Y') ?>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
-    <script src="<?= base_url(); ?>material/js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url() ?>material/js/sweetalert2.all.min.js"></script>
+            </div>
+        </footer>
+    </main>
 </body>
 
 </html>

7687310dd76242acb99a77d4675f534a94345b81
rdtyaandr <radltyaa260819@gmail.com>
2024-07-18 09:38:00 +0700

    revisi design akun 1

--- application/views/v_akun.php
+++ application/views/v_akun.php
@@ -1,18 +1,25 @@
-<link href="<?= base_url() ?>material/css/account.css" rel="stylesheet"/>
+<link href="<?= base_url() ?>material/css/account.css" rel="stylesheet" />
+<script src="<?= base_url() ?>material/js/account.js"></script>
 
 <div class="container-fluid">
     <h1 class="mt-4">Detail Akun</h1>
 
     <div class="row">
         <!-- Profile Card -->
-        <div class="col-xl-4">
-            <div class="card mb-4 shadow-sm">
-                <div class="card-header bg-primary text-white">Profil</div>
+        <div class="col-xl-4 col-lg-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-primary text-white">
+                    <i class="fas fa-user-circle fa-2x mr-2"></i> Profil
+                </div>
                 <div class="card-body text-center">
                     <div class="profile-picture">
-                        <img class="img-account-profile rounded-circle mb-3" src="<?= base_url() ?>material/image/user.png" alt="" style="width: 150px;">
+                        <img class="img-account-profile rounded-circle mb-3"
+                            src="<?= base_url() ?>material/image/user.png" alt="" style="width: 150px;">
                     </div>
-                    <button class="btn btn-sm btn-link text-primary" id="editProfileBtn" data-toggle="modal" data-target="#editProfileModal">Edit Foto</button>
+                    <button class="btn btn-sm btn-link text-primary" id="editProfileBtn" data-toggle="modal"
+                        data-target="#editProfileModal">
+                        <i class="fas fa-camera fa-lg mr-2"></i> Edit Foto
+                    </button>
                     <h4 class="mb-1"><?= $this->session->userdata('nama_lengkap'); ?></h4>
                     <p class="text-muted mb-3"><?= $this->session->userdata('username'); ?></p>
                 </div>
@@ -20,28 +27,45 @@
         </div>
 
         <!-- Account Details Form -->
-        <div class="col-xl-8">
-            <div class="card mb-4 shadow-sm">
-                <div class="card-header bg-primary text-white">Detail Akun</div>
+        <div class="col-xl-8 col-lg-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-primary text-white">
+                    <i class="fas fa-cog fa-2x mr-2"></i> Detail Akun
+                </div>
                 <div class="card-body">
                     <form action="<?= base_url('dashboard/update_account') ?>" method="post">
                         <div class="form-group">
                             <label for="nama_lengkap" class="form-label">Nama Lengkap</label>
-                            <input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap" value="<?= $this->session->userdata('nama_lengkap'); ?>" required>
+                            <input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap"
+                                value="<?= $this->session->userdata('nama_lengkap'); ?>" readonly
+                                onclick="resetPasswordFields()">
                         </div>
                         <div class="form-group">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" name="username" value="<?= $this->session->userdata('username'); ?>" required>
+                            <input type="text" class="form-control" id="username" name="username"
+                                value="<?= $this->session->userdata('username'); ?>" onclick="resetPasswordFields()">
                         </div>
-                        <div class="form-group">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" value="<?= $this->session->userdata('email'); ?>" required>
-                        </div>
-                        <div class="form-group">
+                        <div class="form-group" id="password-group">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Kosongkan jika tidak ingin mengubah password">
+                            <input type="password" class="form-control" id="password" name="password"
+                                placeholder="Kosongkan jika tidak ingin mengubah password"
+                                onclick="showPasswordFields()">
                         </div>
-                        <button type="submit" class="btn btn-primary float-right">Simpan</button>
+                        <div id="newPasswordFields" style="display: none;">
+                            <div class="form-group">
+                                <label for="new_password" class="form-label">Masukkan password baru</label>
+                                <input type="password" class="form-control" id="new_password" name="new_password"
+                                    placeholder="Ketik disini">
+                            </div>
+                            <div class="form-group">
+                                <label for="confirm_password" class="form-label">Masukkan ulang password baru</label>
+                                <input type="password" class="form-control" id="confirm_password"
+                                    name="confirm_password" placeholder="Ketik disini">
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary float-right">
+                            <i class="fas fa-save fa-lg mr-2"></i> Simpan
+                        </button>
                     </form>
                 </div>
             </div>
@@ -50,11 +74,14 @@
 </div>
 
 <!-- Modal Edit Foto Profile -->
-<div class="modal fade" id="editProfileModal" tabindex="-1" role="dialog" aria-labelledby="editProfileModalLabel" aria-hidden="true">
+<div class="modal fade" id="editProfileModal" tabindex="-1" role="dialog" aria-labelledby="editProfileModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
-        <div class="modal-content">
+        <div class="modal-content animate__animated animate__fadeInDown">
             <div class="modal-header">
-                <h5 class="modal-title" id="editProfileModalLabel">Edit Foto Profil</h5>
+                <h5 class="modal-title" id="editProfileModalLabel">
+                    <i class="fas fa-camera fa-lg mr-2"></i> Edit Foto Profil
+                </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -63,16 +90,18 @@
                 <!-- Form untuk upload foto -->
                 <form>
                     <div class="form-group">
-                        <label for="uploadProfile">Pilih Foto Profil</label>
-                        <input type="file" class="form-control-file" id="uploadProfile">
+                        <label for="uploadProfile" class="custom-file-label" id="customFileLabel">Pilih Foto
+                            Profil</label>
+                        <input type="file" class="custom-file-input" id="uploadProfile" onchange="updateFileName(this)">
+                        <span class="custom-file-control" id="selectedFileName"></span>
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                <button type="button" class="btn btn-primary">Simpan</button>
+                <button type="button" class="btn btn-primary">
+                    <i class="fas fa-save fa-lg"></i>
+                </button>
             </div>
         </div>
     </div>
 </div>
-
---
+++ material/js/account.js
@@ -0,0 +1,60 @@
+function showPasswordFields() {
+    const passwordLabel = document.querySelector('label[for="password"]');
+    const passwordInput = document.getElementById('password');
+    const newPasswordFields = document.getElementById('newPasswordFields');
+
+    passwordLabel.innerText = 'Masukkan password lama';
+    passwordInput.placeholder = 'Ketik disini';
+    newPasswordFields.style.display = 'block';
+    setTimeout(() => {
+        newPasswordFields.style.opacity = 1;
+    }, 10);
+}
+
+function resetPasswordFields() {
+    const passwordLabel = document.querySelector('label[for="password"]');
+    const passwordInput = document.getElementById('password');
+    const newPasswordInput = document.getElementById('new_password');
+    const confirmNewPasswordInput = document.getElementById('confirm_password');
+    const newPasswordFields = document.getElementById('newPasswordFields');
+
+    // Cek apakah salah satu dari input password sedang dalam fokus
+    if (document.activeElement === passwordInput || 
+        document.activeElement === newPasswordInput ||
+        document.activeElement === confirmNewPasswordInput) {
+        return; // Jangan menyembunyikan jika salah satu input password sedang dalam fokus
+    }
+
+    // Cek apakah ada nilai yang sudah diisi di input password baru atau konfirmasi password baru
+    if (passwordInput.value.trim() !== '' || 
+        newPasswordInput.value.trim() !== '' || 
+        confirmNewPasswordInput.value.trim() !== '') {
+        return; // Jangan menyembunyikan jika ada nilai yang sudah diisi
+    }
+
+    passwordLabel.innerText = 'Password';
+    passwordInput.placeholder = 'Kosongkan jika tidak ingin mengubah password';
+    newPasswordFields.style.opacity = 0;
+    setTimeout(() => {
+        newPasswordFields.style.display = 'none';
+    }, 300);
+}
+
+document.addEventListener('click', function(event) {
+    const form = document.querySelector('.card-body');
+
+    // Jika yang diklik bukan bagian dari form atau input password, reset fields
+    if (!form.contains(event.target) && event.target.id !== 'password') {
+        resetPasswordFields();
+    }
+});
+
+document.getElementById('password').addEventListener('focus', showPasswordFields);
+document.getElementById('new_password').addEventListener('focus', showPasswordFields);
+document.getElementById('confirm_password').addEventListener('focus', showPasswordFields);
+
+function updateFileName(input) {
+    var fileName = input.files[0].name;
+    var label = document.getElementById('customFileLabel');
+    label.textContent = fileName;
+}
